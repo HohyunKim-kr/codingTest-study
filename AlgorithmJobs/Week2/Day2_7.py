8f2a3d05abe8982bec5d1b3588c7e5ddd47149e2
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-08-18 16:48:06 +0900

    Create Day2_7.py

---
+++ AlgorithmJobs/Week2/Day2_7.py
@@ -0,0 +1,11 @@
+﻿## template
+n=int(input())
+step=0
+for i in range(1,n+1):
+  step+=i
+  kstep=step
+  for j in range(i,n+1):
+    print(kstep,end=' ')
+    kstep+=j
+  print()
+// 파이썬 정답 c++ 로 다시풀기
