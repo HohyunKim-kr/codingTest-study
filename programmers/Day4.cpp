7e6a63147f2e370ba6990bedc9813201b1422d84
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-08-18 14:03:31 +0900

    Create : Day4.cpp
    Day4.cpp -프로그래머스 큰 수 만들기-

---
+++ programmers/Day4.cpp
@@ -0,0 +1,26 @@
+﻿// 프로그래머스 - CML_큰 수 만들기
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = "";
+
+    int numSize = number.size() - k;
+    int start = 0;
+    for (int i = 0; i < numSize; i++) {
+        char maxNum = number[start];
+        int maxIdx = start;
+        for (int j = start; j <= k + i; j++) {
+            if (maxNum < number[j]) {
+                maxNum = number[j];
+                maxIdx = j;
+            }
+        }
+        start = maxIdx + 1;
+        answer += maxNum;
+    }
+
+    return answer;
+}
