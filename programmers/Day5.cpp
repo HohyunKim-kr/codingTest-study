6e92c407b2ba5c06348f1799c8446dcb07832a9c
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-08-19 22:05:21 +0900

    Create Day5.cpp

---
+++ programmers/Day5.cpp
@@ -0,0 +1,25 @@
+﻿// 프로그래머스 가장 큰 수
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(string a, string b)
+{
+    return a + b > b + a;
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+    vector<string> str_numbers;
+    for (auto number : numbers)
+        str_numbers.push_back(to_string(number));
+    sort(str_numbers.begin(), str_numbers.end(), compare);
+    if (str_numbers[0] == "0")
+        return "0";
+    for (auto number : str_numbers)
+        answer += number;
+    return answer;
+}
