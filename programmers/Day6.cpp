9244503517513275b19d5da73507988d7537e7cb
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-08-20 20:52:09 +0900

    Create Day6.cpp

---
+++ programmers/Day6.cpp
@@ -0,0 +1,17 @@
+﻿// 프로그래머스 H - INDEX
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int idx(0);
+    sort(citations.begin(), citations.end());
+
+    for (idx = 0; idx < (citations.size() + 1) / 2; ++idx) {
+        if (citations.size() - idx <= citations[idx]) {
+            return (citations.size() - idx);
+        }
+    }
+}
