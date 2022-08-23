6d811b5b39f66345371607a5bb68d3d83bad9b95
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-08-23 13:15:30 +0900

    Create 오븐시계.cpp

---
+++ BaekjunAlgorithm/step2/오븐시계.cpp
@@ -0,0 +1,33 @@
+﻿// 백준 2525번 오븐 시계
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int A, B, C;
+    int up;
+    int down;
+    scanf("%d %d", &A, &B);
+    scanf("%d", &C);
+    if (B + C < 60)
+    {
+        printf("%d %d", A, B + C);
+    }
+    else
+    {
+        up = (B + C) / 60;
+        down = (B + C) % 60;
+        if (A + up >= 24)
+        {
+            printf("%d %d", A + up - 24, down);
+        }
+        else
+        {
+            printf("%d %d", A + up, down);
+        }
+
+    }
+    return 0;
+}
