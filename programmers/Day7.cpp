e3c945d620417822821283352fef264102167a22
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-08-23 16:09:16 +0900

    Create Day7.cpp

---
+++ programmers/Day7.cpp
@@ -0,0 +1,17 @@
+﻿// 프로그래머스 Lv1 직사각형 별찍기
+
+#include <stdio.h>
+
+int main(void) {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
