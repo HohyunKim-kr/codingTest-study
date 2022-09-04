091543980da6baf0a977d2acfbd18065b014c493
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-09-04 23:14:08 +0900

    Create 2739.cpp

---
+++ BaekjunAlgorithm/step3/2739.cpp
@@ -0,0 +1,15 @@
+﻿// 백준 2739번
+#include<stdio.h>
+int main()
+{
+	int N;
+	int i;
+	scanf("%d", &N);
+	for (i = 1; i <= 9; i++) {
+		{
+			printf("%d * %d = %d", N, i, N * i);
+		}printf("\n");
+	}
+	return 0;
+
+}
