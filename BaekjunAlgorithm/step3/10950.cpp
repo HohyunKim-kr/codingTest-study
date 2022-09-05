d415d728c5920e94a6e593ccfb9705df5df3f734
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-09-05 21:10:34 +0900

    Create 10950.cpp

---
+++ BaekjunAlgorithm/step3/10950.cpp
@@ -0,0 +1,21 @@
+﻿// 백준 알고리즘 	10950번 java 예전에 푼거라 자바로 풀었음.
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		int T, A, B;
+		Scanner scanner = new Scanner(System.in);
+		T = scanner.nextInt();
+		int[] sum = new int[T];
+		for (int i = 0; i < T; i++) {
+			A = scanner.nextInt();
+			B = scanner.nextInt();
+			sum[i] = A + B;
+		}
+		for (int i = 0; i < T; i++) {
+			System.out.println(sum[i]);
+		}
+
+	}
+}
