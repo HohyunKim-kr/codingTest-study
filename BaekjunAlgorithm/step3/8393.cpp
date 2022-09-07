04bae82e1b602fedf829f75a02d893698e6e26a9
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-09-07 21:15:52 +0900

    Create 8393.cpp

---
+++ BaekjunAlgorithm/step3/8393.cpp
@@ -0,0 +1,15 @@
+﻿// 백준 알고리즘 8393번 자바로 풀었음
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int n;
+		int sum = 0;
+		Scanner scanner = new Scanner(System.in);
+		n = scanner.nextInt();
+		for (int i = 0; i <= n; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+}
