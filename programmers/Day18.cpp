761f17136ce1b6a4f126f5854d992ce538b07ac1
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-09-20 01:54:04 +0900

    Create Day18.cpp

---
+++ programmers/Day18.cpp
@@ -0,0 +1,12 @@
+﻿// 프로그래머스 LV1 짝수와 홀수 
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// c언어 삼항 연산자로 푸는 것이 더 좋은듯...
+
+string solution(int num) {
+    if (num % 2 != 0) return "Odd";
+    else return "Even";
+}
