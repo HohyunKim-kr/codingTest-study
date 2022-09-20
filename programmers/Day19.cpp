620103d360f83a075aba0730432db549edf2a6c6
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-09-21 00:46:43 +0900

    Create Day19.cpp

---
+++ programmers/Day19.cpp
@@ -0,0 +1,16 @@
+﻿// 프로그래머스 LV1 자릿수 더하기
+#include <iostream>
+#include <string>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+
+    while (n != 0)
+    {
+        answer = answer + n % 10;
+        n = n / 10;
+    }
+    return answer;
+}
