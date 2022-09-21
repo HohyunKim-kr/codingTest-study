4d95c8bd830209fc97228977b0fae4e217597955
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-09-22 00:47:12 +0900

    Create Day20.cpp

---
+++ programmers/Day20.cpp
@@ -0,0 +1,20 @@
+﻿// 프로그래머스 LV1 평균구하기 벡터 + 반복자 사용
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    double sum = 0;
+
+    for (auto iter = arr.begin(); iter != arr.end(); ++iter)
+    {
+        sum += *iter;
+    }
+    answer = sum / arr.size();
+
+    return answer;
+}
