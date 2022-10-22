cd4a97937541509f9bfeac7926de97f3b01c8198
Hohyun.Kim <62325545+HohyunKim-kr@users.noreply.github.com>
2022-10-22 17:31:01 +0900

    Create InputAndOutput.cpp

---
+++ HackerRank/InputAndOutput.cpp
@@ -0,0 +1,20 @@
+// HackRank Input and Ouput
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    string s;
+    int x, y, z, answer;
+    cin >> x >> y >> z;
+    answer = x + y + z;
+    printf("%d", answer);
+}
+
+
